a47fb753f505ba69182c635cfe728b8bff2a15e1
User <tharindu2003wxyz@gmail.com>
2026-02-25 04:37:08 +0530

    5st commit

--- app/src/main/java/com/codex/foodcaf/activity/MainActivity.java
+++ app/src/main/java/com/codex/foodcaf/activity/MainActivity.java
@@ -1,44 +1,67 @@
 package com.codex.foodcaf.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.bumptech.glide.Glide;
 import com.codex.foodcaf.R;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, NavigationBarView.OnItemSelectedListener {
+
+     private DrawerLayout     drawerLayout;
+    private MaterialToolbar toolbar;
+    private NavigationView navigationView;
+
+    private ImageView imageView;
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawertLayout), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-//      ImageView imageView = findViewById(R.id.profileImage);
-//
-//
-//
-//        Glide.with(this)
-//                .asBitmap()
-//                .load(R.drawable.profile)
-//                .override(80)
-//                .into(imageView);
 
-        ImageView imageView = findViewById(R.id.nav_Logo);
         ImageView menuicon = findViewById(R.id.menu_Logo);
 
+        drawerLayout = findViewById(R.id.drawertLayout);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.sideNavView);
+        bottomNavigationView = findViewById(R.id.bottomNavView);
 
+
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.setDrawerIndicatorEnabled(false);
+        toggle.syncState();
+
+
+
+
+        ImageView imageView = findViewById(R.id.nav_Logo);
 
         Glide.with(this)
                 .asBitmap()
@@ -52,6 +75,44 @@ public class MainActivity extends AppCompatActivity {
                 .override(70)
                 .into(menuicon);
 
+        menuicon.setOnClickListener(view -> {
+            drawerLayout.openDrawer(androidx.core.view.GravityCompat.START);
+        });
+
+
+        getOnBackPressedDispatcher().addCallback(this,new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+//                Toast.makeText(MainActivity.this, "Back pressed", Toast.LENGTH_SHORT).show();
+                if(drawerLayout.isDrawerOpen(androidx.core.view.GravityCompat.START)){
+                    drawerLayout.closeDrawer(androidx.core.view.GravityCompat.START);
+                }else{
+                    finish();
+                }
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(this);
+        bottomNavigationView.setOnItemSelectedListener(this);
     }
 
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.side_nav_home | R.id.bottom_nav_home:
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.side_nav_profile | R.id.bottom_profile:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+                case R.id.side_nav_favorite:
+                Toast.makeText(this, "Favorite", Toast.LENGTH_SHORT).show();
+                break;
+
+
+        }
+
+        return false;
+    }
 }
--- app/src/main/java/com/codex/foodcaf/activity/SpalshActivity.java
+++ app/src/main/java/com/codex/foodcaf/activity/SpalshActivity.java
@@ -15,7 +15,9 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import com.bumptech.glide.Glide;
 import com.codex.foodcaf.R;
@@ -23,63 +25,105 @@ import com.squareup.picasso.Picasso;
 
 public class SpalshActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
-            getWindow().setDecorFitsSystemWindows(false);
-
-            WindowInsetsController Controller = getWindow().getInsetsController();
-            if (Controller != null) {
-                Controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-            }
-        }else {
-            getWindow().setFlags(
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN
-            );
-        }
-        setContentView(R.layout.activity_spalsh);
-
-        ImageView imageView = findViewById(R.id.spalshLogo);
-
-//        Picasso.get()
-//                .load(R.drawable.ic_launcher_foreground)
-//                .resize(300,300)
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+//
+////        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+////            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+////            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+////            return insets;
+////        });
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+//            getWindow().setDecorFitsSystemWindows(false);
+//
+//            WindowInsetsController Controller = getWindow().getInsetsController();
+//            if (Controller != null) {
+//                Controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+//            }
+//        }else {
+//            getWindow().setFlags(
+//                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+//            );
+//        }
+//        setContentView(R.layout.activity_spalsh);
+//
+//        ImageView imageView = findViewById(R.id.spalshLogo);
+//
+////        Picasso.get()
+////                .load(R.drawable.ic_launcher_foreground)
+////                .resize(300,300)
+////                .into(imageView);
+//
+//        Glide.with(this)
+//                .asBitmap()
+//                .load(R.drawable.applogo)
+//                .override(250)
 //                .into(imageView);
+//
+//        new Handler(Looper.getMainLooper())
+//                .postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        findViewById(R.id.progress_circular).setVisibility(View.VISIBLE);
+//
+//                    }
+//                },500);
+//
+//        new Handler(Looper.getMainLooper())
+//                .postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        findViewById(R.id.progress_circular).setVisibility(View.INVISIBLE);
+//                        Intent intent = new Intent(SpalshActivity.this, MainActivity.class);
+//                        startActivity(intent);
+//                        finish();
+//                    }
+//                },3000);
+//    }
 
-        Glide.with(this)
-                .asBitmap()
-                .load(R.drawable.applogo)
-                .override(250)
-                .into(imageView);
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    EdgeToEdge.enable(this);
 
-        new Handler(Looper.getMainLooper())
-                .postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        findViewById(R.id.progress_circular).setVisibility(View.VISIBLE);
+    // Modern AndroidX way to handle fullscreen / hiding system bars without deprecation warnings
+    WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    WindowInsetsControllerCompat windowInsetsController =
+            WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
 
-                    }
-                },500);
-
-        new Handler(Looper.getMainLooper())
-                .postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        findViewById(R.id.progress_circular).setVisibility(View.INVISIBLE);
-                        Intent intent = new Intent(SpalshActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                },3000);
+    if (windowInsetsController != null) {
+        // Hides both the status bar and navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
+        // Allows users to swipe to temporarily see the system bars
+        windowInsetsController.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
     }
+
+    setContentView(R.layout.activity_spalsh);
+
+    ImageView imageView = findViewById(R.id.spalshLogo);
+
+    Glide.with(this)
+            .asBitmap()
+            .load(R.drawable.applogo)
+            .override(250)
+            .into(imageView);
+
+    // I also shortened your Runnables to lambdas to make the code a bit cleaner
+    new Handler(Looper.getMainLooper()).postDelayed(() -> {
+        findViewById(R.id.progress_circular).setVisibility(View.VISIBLE);
+    }, 500);
+
+    new Handler(Looper.getMainLooper()).postDelayed(() -> {
+        findViewById(R.id.progress_circular).setVisibility(View.INVISIBLE);
+        Intent intent = new Intent(SpalshActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }, 3000);
+}
 
 }
