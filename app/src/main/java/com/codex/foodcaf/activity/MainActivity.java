3874f8ebe659c433486bf5f63637bb33756bf6d4
User <tharindu2003wxyz@gmail.com>
2026-02-25 01:56:08 +0530

    3st commit

--- app/src/main/java/com/codex/foodcaf/activity/MainActivity.java
+++ app/src/main/java/com/codex/foodcaf/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.codex.foodcaf.activity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
 import com.codex.foodcaf.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,10 +19,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawertLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+//      ImageView imageView = findViewById(R.id.profileImage);
+//
+//
+//
+//        Glide.with(this)
+//                .asBitmap()
+//                .load(R.drawable.profile)
+//                .override(80)
+//                .into(imageView);
+
+        ImageView imageView = findViewById(R.id.nav_Logo);
+        ImageView menuicon = findViewById(R.id.menu_Logo);
+
+
+
+        Glide.with(this)
+                .asBitmap()
+                .load(R.drawable.applogo)
+                .override(100)
+                .into(imageView);
+
+        Glide.with(this)
+                .asBitmap()
+                .load(R.drawable.more_vert_24px)
+                .override(70)
+                .into(menuicon);
+
     }
+
 }
