ebe5a6516240449ce8ceeef35c0649375be00d25
User <tharindu2003wxyz@gmail.com>
2026-03-01 00:59:09 +0530

    17st commit

--- app/src/main/java/com/codex/foodcaf/model/Category.java
+++ app/src/main/java/com/codex/foodcaf/model/Category.java
@@ -1,4 +1,18 @@
 package com.codex.foodcaf.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
+    private  String categoryId;
+    private  String categoryName;
+    private  String categoryImage;
+    private  String categorySubtitle;
 }
